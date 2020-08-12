2af64c1138b83ec0b243d927208d299de2fcffbd
Yu-jinKim <yujin.kim@hotmail.fr>
2020-08-12 14:57:10 +0100

    comments + add prefix to fields if necessary

--- src/vcf_annotator.sh
+++ src/vcf_annotator.sh
@@ -16,6 +16,7 @@ main() {
     dx download "$raw_vcf"
     dx download "$src_vcf"
 
+    # Compile bcftools
     tar xvjf bcftools-1.10.2.tar.bz2
     
     cd bcftools-1.10.2
@@ -26,16 +27,36 @@ main() {
 
     cd ..
 
+    # Bgzip the raw vcf
     bcftools view $raw_vcf_name -Oz > ${raw_vcf_name}.gz
 
+    # Index both given vcfs
     bcftools index ${raw_vcf_name}.gz
     bcftools index ${src_vcf_name}
 
+    # define file output name
     basename=$(echo $raw_vcf_name | cut -d"." -f1)
     annotated_vcf_file=${basename}_${output_suffix}.vcf
     annotated_vcf_bgzip_file=${annotated_vcf_file}.gz
 
-    bcftools annotate -a $src_vcf_name -c $fields ${raw_vcf_name}.gz > $annotated_vcf_file
+    # add prefix to fields that need them
+    fields_array=()
+    IFS=","
+    for field in $fields; do
+        if [[ $field != *":="* ]]; then
+            field_to_add="EGG_${field}:=${field}"
+        else
+            field_to_add=$field
+        fi
+        
+        fields_array+=($field_to_add)
+    done
+
+    new_fields=$(IFS=","; echo "${fields_array[*]}")
+    IFS=""
+
+    # Annotate and bgzip output
+    bcftools annotate -a $src_vcf_name -c $new_fields ${raw_vcf_name}.gz > $annotated_vcf_file
     bcftools view ${annotated_vcf_file} -Oz > $annotated_vcf_bgzip_file
 
     annotated_vcf=$(dx upload $annotated_vcf_bgzip_file --brief)
